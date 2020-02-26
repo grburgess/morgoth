da45d51210d27fa78ffc85ab4837b0a316293966
grburgess <jmichaelburgess@gmail.com>
2020-02-26 21:56:27 +0100

    add a small test

--- morgoth/test/test_pipeline.py
+++ morgoth/test/test_pipeline.py
@@ -1,7 +1,8 @@
 import luigi
 import os
 import shutil
-os.environ['GBM_TRIGGER_DATA_DIR'] = './'
+
+os.environ["GBM_TRIGGER_DATA_DIR"] = "./"
 import lxml.etree
 import time
 from morgoth.trigger import parse_trigger_file_and_write, OpenGBMFile
@@ -15,23 +16,21 @@ from morgoth.configuration import morgoth_config
 for i in range(3):
 
     v = f"v0{i}"
-    morgoth_config['download']['trigdat'][v]['max_time'] = 10
+    morgoth_config["download"]["trigdat"][v]["max_time"] = 10
 
 
-# def test_parse_trigger():
+def test_parse_trigger():
 
-#     ff = get_path_of_data_file("gbm_flt.xml")
-#     with open(ff, "r") as f:
-#         root = lxml.etree.parse(f)
+    ff = get_path_of_data_file("gbm_flt.xml")
+    with open(ff, "r") as f:
+        root = lxml.etree.parse(f)
 
-#     grb = parse_trigger_file_and_write(root)
+    grb = parse_trigger_file_and_write(root)
 
-    
-
-    
-#     assert luigi.build(
-#         [OpenGBMFile(grb=grb)], local_scheduler=True
-#     )
+    assert luigi.build([OpenGBMFile(grb=grb)],
+                       local_scheduler=False,
+                       scheduler_host="localhost")
+                       
 
 
 # def test_download_trigdat():
@@ -42,7 +41,7 @@ for i in range(3):
 
 #     grb = parse_trigger_file_and_write(root)
 
-    
+
 #     assert luigi.build(
 #         [DownloadTrigdat(grb_name=grb, version='v01')], local_scheduler=False,
 #         scheduler_host='localhost'
@@ -59,13 +58,14 @@ def test_pipeline():
 
     time.sleep(5)
     luigi.build(
-        [CreateAllPages(grb_name=grb)], local_scheduler=False,
-        scheduler_host='localhost', workers=4
+        [CreateAllPages(grb_name=grb)],
+        local_scheduler=False,
+        scheduler_host="localhost",
+        workers=4,
     )
     # luigi.build(
     #     [CreateAllPages(grb_name=grb)], local_scheduler=True,
     #     workers=6, no_lock=False
     # )
 
-    
     shutil.rmtree(grb)
