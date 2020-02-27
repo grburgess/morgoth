5b338f7376f8f3ea7140b3afbf2cfe5dfc53c49e
grburgess <jmichaelburgess@gmail.com>
2020-02-27 07:43:25 +0100

    make file more complete

--- morgoth/utils/package_data.py
+++ morgoth/utils/package_data.py
@@ -3,16 +3,18 @@ import os
 from shutil import copyfile
 
 
-def get_path_of_data_file(data_file):
-    file_path = pkg_resources.resource_filename("morgoth", "data/%s" % data_file)
-
-    return file_path
-
-
 def get_path_of_data_dir():
     file_path = pkg_resources.resource_filename("morgoth", "data")
 
     return file_path
+
+
+def get_path_of_data_file(data_file):
+    
+    file_path = os.path.join(get_path_of_data_dir(), data_file)
+
+    return file_path
+
 
 
 def copy_package_data(data_file):
