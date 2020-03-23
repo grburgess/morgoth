72d463f4d7d1a7e77b5cdfd930861cebe96b8ca5
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2020-03-23 14:52:39 +0100

    Add time_selection_handler

---
+++ morgoth/time_selection_handler.py
@@ -0,0 +1,33 @@
+import luigi
+import os
+
+from morgoth.downloaders import DownloadTrigdat
+from auto_loc.fit.time_selection import TimeSelection
+
+base_dir = os.environ.get("GBM_TRIGGER_DATA_DIR")
+
+class TimeSelectionHandler(luigi.ExternalTask):
+    grb_name = luigi.Parameter()
+    version = luigi.Parameter(default="v01")#TODO change this to v00 for not testing!!!!!!!!
+
+    
+    def requires(self):
+
+        return DownloadTrigdat(grb_name=self.grb_name, version=self.version) 
+
+    def output(self):
+        
+        filename = "time_selection.json"
+
+        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
+
+    def run(self):
+
+        time_selection = TimeSelection(self.grb_name, self.version)
+        
+        filename = "time_selection.json"
+
+        tmp = os.path.join(base_dir, self.grb_name, filename) 
+        
+        time_selection.save_json(tmp)
+        
