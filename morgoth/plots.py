8e68552232f5c3d785a920652bdf8ebc14955e0b
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-03-26 18:15:52 +0100

    Add Dummy Create for Spectrum plot

--- morgoth/plots.py
+++ morgoth/plots.py
@@ -173,3 +173,22 @@ class CreateSatellitePlot(luigi.Task):
             ra=result['localization']['ra'],
             dec=result['localization']['dec'],
         )
+
+
+class CreateSpectrumPlot(luigi.Task):
+    grb_name = luigi.Parameter()
+    report_type = luigi.Parameter()
+    version = luigi.Parameter(default="v00")
+
+    def requires(self):
+        return ProcessFitResults(grb_name=self.grb_name, report_type=self.report_type, version=self.version)
+
+    def output(self):
+        filename = f"{self.grb_name}_spectrum_plot_{self.report_type}_{self.version}.png"
+        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, self.report_type, self.version, 'plots', filename))
+
+    def run(self):
+        filename = f"{self.grb_name}_spectrum_plot_{self.report_type}_{self.version}.png"
+        tmp = os.path.join(base_dir, self.grb_name, self.report_type, self.version, 'plots', filename)
+
+        os.system(f"touch {tmp}")
