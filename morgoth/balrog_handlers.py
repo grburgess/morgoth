f4c5fef6be7d12c7d606ad7b876958d27a6f8bd3
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2020-03-26 17:10:33 +0100

    added balrog fits

--- morgoth/balrog_handlers.py
+++ morgoth/balrog_handlers.py
@@ -1,10 +1,14 @@
 import luigi
+from luigi.contrib.external_program import ExternalPythonProgramTask
 import os
 
 from morgoth.bkg_fit_handler import BackgroundFitTTE, BackgroundFitTrigdat
+from morgoth.configuration import morgoth_config
 
 base_dir = os.environ.get("GBM_TRIGGER_DATA_DIR")
 
+n_cores_multinest = morgoth_config["multinest"]["n_cores"]
+path_to_python = morgoth_config["multinest"]["path_to_python"]
 
 _gbm_detectors = (
     "n0",
@@ -46,12 +50,12 @@ class ProcessFitResults(luigi.Task):
 
     def output(self):
 
-        filename = f"{self.report_type}_{self.version}_report.txt"
+        filename = f"{self.report_type.lower()}_{self.version}_report.txt"
         return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
 
     def run(self):
 
-        filename = f"{self.report_type}_{self.version}_report.txt"
+        filename = f"{self.report_type.lower()}_{self.version}_report.txt"
         tmp = os.path.join(base_dir, self.grb_name, filename)
 
         os.system(f"touch {tmp}")
@@ -63,40 +67,46 @@ class RunBalrogTTE(luigi.ExternalTask):
 
     def requires(self):
 
-        return [BackgroundFitTTE(grb_name=self.grb_name, version=self.version, detector=d) for d in _gbm_detectors]
+        return BackgroundFitTTE(grb_name=self.grb_name, version=self.version)#[BackgroundFitTTE(grb_name=self.grb_name, version=self.version, detector=d) for d in _gbm_detectors]
         
-
     def output(self):
-
-        filename = f"tte_{self.version}_fit.txt"
-        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
+        
+        filename = f"tte_{self.version}_loc_results.fits"
+        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, "fit_results", filename))
 
     def run(self):
 
-        filename = f"tte_{self.version}_fit.txt"
-        tmp = os.path.join(base_dir, self.grb_name, filename)
+        fit_script_path = f"{os.path.dirname(os.path.abspath(__file__))}/auto_loc/fit_script.py"
 
-        os.system(f"touch {tmp}")
+        time_selection_file_path = os.path.join(base_dir, self.grb_name, "time_selection.yml")
+
+        bkg_fit_file_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_tte_{self.version}.yml")
+        
+        os.system(f"mpiexec -n {n_cores_multinest} {path_to_python} {fit_script_path} {self.grb_name} {self.version} {bkg_fit_file_path} {time_selection_file_path} tte")
 
 
-class RunBalrogTrigdat(luigi.ExternalTask):
+class RunBalrogTrigdat(luigi.ExternalTask):# ExternalPythonProgramTask):
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
 
+    #def program_args(self):
+    #    return ['./auto_loc/fit_script.py', n_cores_multinest, self.grb_name, self.version, bkg_fit_file_path, time_selection_file_path, 'trigdat', path_to_python]
+    
     def requires(self):
 
         return BackgroundFitTrigdat(grb_name=self.grb_name, version=self.version)
 
     def output(self):
-
-        filename = f"trigdat_{self.version}_fit.txt"
-        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
+        filename = f"trigdat_{self.version}_loc_results.fits"
+        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, "fit_results",filename))
 
     def run(self):
 
-        filename = f"trigdat_{self.version}_fit.txt"
-        tmp = os.path.join(base_dir, self.grb_name, filename)
+        fit_script_path = f"{os.path.dirname(os.path.abspath(__file__))}/auto_loc/fit_script.py"
 
-        os.system(f"touch {tmp}")
+        time_selection_file_path = os.path.join(base_dir, self.grb_name, "time_selection.yml")
+
+        bkg_fit_file_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_trigdat_{self.version}.yml")
         
+        os.system(f"mpiexec -n {n_cores_multinest} {path_to_python} {fit_script_path} {self.grb_name} {self.version} {bkg_fit_file_path} {time_selection_file_path} trigdat")
 
