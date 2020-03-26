9edd7dce1262bf85f40b672c57d20cfd97d6f056
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2020-03-26 17:09:58 +0100

    added automatic background fit

---
+++ morgoth/auto_loc/bkg_fit.py
@@ -0,0 +1,393 @@
+from trigdat_reader import *
+import numpy as np
+import yaml
+import os
+
+from threeML.utils.time_series.event_list import EventListWithDeadTime
+from threeML.utils.data_builders.fermi.gbm_data import GBMTTEFile
+from threeML.utils.data_builders.time_series_builder import TimeSeriesBuilder
+from threeML.utils.spectrum.binned_spectrum import BinnedSpectrum, BinnedSpectrumWithDispersion
+
+import gbm_drm_gen as drm
+from gbm_drm_gen.io.balrog_drm import BALROG_DRM
+
+base_dir = os.environ.get("GBM_TRIGGER_DATA_DIR") 
+
+_gbm_detectors = ['n0','n1','n2','n3','n4','n5','n6','n7','n8','n9','na','nb','b0','b1']
+
+class BkgFittingTrigdat(object):
+
+    def __init__(self, grb_name, version, time_selection_file_path):
+        """
+        Object used for fitting of the background in every detector and echan.
+        :param grb_name: Name of GRB
+        :param version: Version number of data
+        :param time_selection_file_path: Path to yaml file with time selection information
+        """
+        self._grb_name = grb_name
+        self._version = version
+        self._time_selection_file_path = time_selection_file_path
+
+        self._build_bkg_plugins()
+        self._choose_dets()
+        
+    def _build_bkg_plugins(self):
+        """
+        Build the background plugins for all dets
+        :return:
+        """
+        
+        trigger_path = os.path.join(base_dir, self._grb_name, f"glg_trigdat_all_bn{self._grb_name[3:]}_{self._version}.fit")
+
+        # Time selection from yaml file
+        with open(self._time_selection_file_path, 'r') as f:
+            data = yaml.load(f)
+            active_time = data['Active_Time']
+            background_time_neg = data['Background_Time']['Time_Before']
+            background_time_pos = data['Background_Time']['Time_After']
+            poly_order = data['Poly_Order']
+            
+        self._trig_reader = TrigReader(trigger_path,
+                                 fine=False,
+                                 verbose=False,
+                                 poly_order=poly_order)
+        
+        self._trig_reader.set_active_time_interval(active_time)
+        
+        self._trig_reader.set_background_selections(background_time_neg,
+                                              background_time_pos)
+
+        self._trigdat_time_series = self._trig_reader._time_series
+
+        self._trigdat_plugins = self._trig_reader.to_plugin(*_gbm_detectors)
+        
+    def save_lightcurves(self, dir_path):
+        """
+        Save plots of the lightcurves for all dets 
+        :param dir_path: Directory path where to save the plots
+        :return:
+        """
+
+        # Max time from yaml file
+        with open(self._time_selection_file_path, 'r') as f:
+            data = yaml.load(f)
+            max_time = data['Max_Time']
+
+        if not os.path.isdir(dir_path):
+
+            os.mkdir(dir_path)
+            
+        self._lightcurve_plots_dir_path = dir_path
+
+        plots = self._trig_reader.view_lightcurve(start=-150, stop=float(max_time), return_plots=True)
+
+        for name, fig in plots:
+            fig.savefig(os.path.join(dir_path, f"{self._grb_name}_lightcurve_trigdat_detector_{name}_plot_{self._version}.png"), bbox_inches='tight')
+                
+    def save_bkg_file(self, dir_path):
+        """
+        Save the hdf5 file with background polynom information        
+        :param dir_path: Directory path where to save the bkg h5 file
+        :return:
+        """
+
+        if not os.path.isdir(dir_path):
+            
+            os.mkdir(dir_path)
+                             
+        self._bkg_fits_dir_path = dir_path
+        
+        for d in _gbm_detectors:
+            
+            self._trigdat_time_series[d].save_background(os.path.join(dir_path,
+                                                                      f'bkg_det{d}.h5'))
+    def _choose_dets(self):
+        """
+        Function to automatically choose the detectors which should be used in the fit
+        :return:
+        """
+        # create plugin with the detectors you wish to use for the fit
+        sign_list = []
+        ndet = 7
+
+        # get significance of all the detectors
+        for i, det in enumerate(self._trigdat_plugins):
+            sign_list.append([det.significance, i])
+        sign_list = np.array(sign_list)
+        
+        #delete nan entries
+        sign_list = sign_list[~np.isnan(sign_list[:,0])]
+        
+        # get index with the most significance
+        sign_list_sort = sign_list[sign_list[:, 0].argsort()]
+        
+        index_sign_max = sign_list_sort[-1, 1]
+
+        side_1_indices = [0, 1, 2, 3, 4, 5, 12]
+        side_2_indices = [6, 7, 8, 9, 10, 11, 13]
+        
+        # only use the detectors on the same side as the detector with the most significance
+        if index_sign_max in side_1_indices:
+            
+            self._use_dets = side_1_indices
+            
+        else:
+            self._use_dets = side_2_indices
+            
+        print('Detectors for fit: ', self._use_dets)
+
+    def save_yaml(self, path):
+        """
+        Save yaml with needed information.
+        :param path: Path where to save the yaml
+        :return:
+        """
+        bkg_fit_dict = {}
+        bkg_fit_dict['Use_dets'] = self._use_dets
+        bkg_fit_dict['Bkg_Fits_Dir_Path'] = self._bkg_fits_dir_path
+        bkg_fit_dict['Lightcurve_Plots_Dir_Path']=self._lightcurve_plots_dir_path
+
+        with open(path, "w") as outfile: 
+            yaml.dump(bkg_fit_dict, outfile) 
+
+    @property
+    def use_dets(self):
+
+        return self._use_dets
+
+    def set_used_dets(self, det_list):
+        """
+        Supports both det countings. 10-13 and n0-b1.
+        """
+        det_list_final=[]
+        for det in det_list:
+            print(det)
+            if type(det)==int:
+
+                det_list_final.append(det)
+                
+            elif 'n' in det:
+                
+                if det[1]=='a':
+                    
+                    det_list_final.append(10)
+                    
+                elif det[1]=='b':
+                    
+                    det_list_final.append(11)
+                    
+                else:
+                    
+                    det_list_final.append(int(det[1]))
+
+            elif 'b0' in det:
+                
+                det_list_final.append(12)
+                
+            elif 'b1' in det:
+                
+                det_list_final.append(13)
+
+            else:
+
+                raise Exception('Wrong format for detector selection')
+
+        self._use_dets = det_list_final
+
+
+class BkgFittingTTE(object):
+
+    def __init__(self, grb_name, version, time_selection_file_path, bkg_fitting_file_path):
+        """
+        Object used for fitting of the background in every detector and echan for tte data.
+        :param grb_name: Name of GRB
+        :param version: Version number of data
+        :param time_selection_file_path: Path to yaml file with time selection information
+        :param bkg_fitting_file_path: Path to yaml file with information for trigdat bkg fitting
+        """
+        self._grb_name = grb_name
+        self._version = version
+        self._time_selection_file_path = time_selection_file_path
+        self._trigdat_bkg_fitting_path = bkg_fitting_file_path
+        
+        self._build_bkg_plugins()
+        #self._choose_dets()
+        
+    def _build_bkg_plugins(self):
+        """
+        Build the background plugins for all dets
+        :return:
+        """
+        
+        # Time selection from yaml file
+        with open(self._time_selection_file_path, 'r') as f:
+            data = yaml.load(f)
+            active_time = data['Active_Time']
+            background_time_neg = data['Background_Time']['Time_Before']
+            background_time_pos = data['Background_Time']['Time_After']
+            poly_order = data['Poly_Order']
+
+        det_ts = []
+
+        for i in range(3):
+            trigdat_file = os.path.join(base_dir, self._grb_name, f"glg_trigdat_all_bn{self._grb_name[3:]}_v0{i}.fit")
+            if os.path.exists(trigdat_file):
+                break
+            
+        for det in _gbm_detectors:
+            
+            tte_file = f"{base_dir}/{self._grb_name}/glg_tte_{det}_bn{self._grb_name[3:]}_{self._version}.fit"
+            cspec_file = f"{base_dir}/{self._grb_name}/glg_cspec_{det}_bn{self._grb_name[3:]}_{self._version}.pha"
+
+            # Response Setup
+            
+            rsp = BALROG_DRM(drm.DRMGenTTE(tte_file=tte_file, trigdat=trigdat_file, mat_type=2, cspecfile=cspec_file), 0.0, 0.0)
+            
+            # Time Series
+            gbm_tte_file = GBMTTEFile(tte_file)
+            event_list = EventListWithDeadTime(arrival_times=gbm_tte_file.arrival_times - \
+                                               gbm_tte_file.trigger_time,
+                                               measurement=gbm_tte_file.energies,
+                                               n_channels=gbm_tte_file.n_channels,
+                                               start_time=gbm_tte_file.tstart - \
+                                               gbm_tte_file.trigger_time,
+                                               stop_time=gbm_tte_file.tstop - \
+                                               gbm_tte_file.trigger_time,
+                                               dead_time=gbm_tte_file.deadtime,
+                                               first_channel=0,
+                                               instrument=gbm_tte_file.det_name,
+                                               mission=gbm_tte_file.mission,
+                                               verbose=True)
+
+            ts = TimeSeriesBuilder(det,
+                                   event_list,
+                                   response=rsp,
+                                   poly_order=poly_order,
+                                   unbinned=False,
+                                   verbose=True,
+                                   container_type=BinnedSpectrumWithDispersion)
+
+            ts.set_background_interval(background_time_neg, background_time_pos)
+            ts.set_active_time_interval(active_time) 
+            det_ts.append(ts)
+
+        # Get start and stop of active
+            
+        #split_activetime = active_time.split('-')
+        #if split_activetime[0]=='':
+        #    start = -float(split_activetime[1])
+        #    if split_activetime[2]=='':
+        #        stop = -float(split_activetime[3])
+        #    else:
+        #        stop = float(split_activetime[2])
+        #else:
+        #    start = float(split_activetime[0])
+        #    stop = float(split_activetime[1])
+        
+        #det_ts[-1].create_time_bins(start=start,stop=stop, method='constant', dt=0.1)
+        #tstart = ts.bins.starts
+        #tstop = ts.bins.stops
+        #active_time = '{}-{}'.format(tstart[0],tstop[0])
+        #det_ts[-1].set_active_time_interval(active_time)
+        #for ts in det_ts[:-1]:
+        #    ts.set_active_time_interval(active_time)
+        #    ts.create_time_bins(start=tstart, stop=tstop, method='custom')
+
+        self._ts = det_ts
+            
+    def save_lightcurves(self, dir_path):
+        """
+        Save plots of the lightcurves for all dets 
+        :param dir_path: Directory path where to save the plots
+        :return:
+        """
+        # Max time from yaml file
+        with open(self._time_selection_file_path, 'r') as f:
+            data = yaml.load(f)
+            max_time = data['Max_Time']
+
+        if not os.path.isdir(dir_path):
+
+            os.mkdir(dir_path)
+            
+        self._lightcurve_plots_dir_path = dir_path
+
+        for det, ts in zip(_gbm_detectors,self._ts):
+            fig = ts.view_lightcurve(start=-150, stop=float(max_time))
+            fig.savefig(os.path.join(dir_path, f"{self._grb_name}_lightcurve_tte_detector_{det}_plot_{self._version}.png"), dpi=350, bbox_inches='tight')
+        
+    def save_bkg_file(self, dir_path):
+        """
+        Save the hdf5 file with background polynom information        
+        :param dir_path: Directory path where to save the bkg h5 file
+        :return:
+        """
+                        
+        if not os.path.isdir(dir_path):
+            
+            os.mkdir(dir_path)
+                             
+        self._bkg_fits_dir_path = dir_path
+        
+        for i, d in enumerate(_gbm_detectors):
+            
+            self._ts[i].save_background(os.path.join(dir_path,f'bkg_det{d}.h5'))
+
+    def save_yaml(self, path):
+        """
+        Save yaml with needed information.
+        :param path: Path where to save the yaml
+        :return:
+        """
+        bkg_fit_dict = {}
+        bkg_fit_dict['Bkg_Fits_Dir_Path'] = self._bkg_fits_dir_path
+        bkg_fit_dict['Lightcurve_Plots_Dir_Path']=self._lightcurve_plots_dir_path
+        with open(self._trigdat_bkg_fitting_path, "r") as f:
+            bkg_fit_dict["Use_dets"] = yaml.load(f)["Use_dets"]
+
+        with open(path,"w") as f:
+            yaml.dump(bkg_fit_dict, f)
+            
+    @property
+    def use_dets(self):
+
+        return self._use_dets
+
+    def set_used_dets(self, det_list):
+        """
+        Supports both det countings. 10-13 and n0-b1.
+        """
+        det_list_final=[]
+        for det in det_list:
+            print(det)
+            if type(det)==int:
+
+                det_list_final.append(det)
+                
+            elif 'n' in det:
+                
+                if det[1]=='a':
+                    
+                    det_list_final.append(10)
+                    
+                elif det[1]=='b':
+                    
+                    det_list_final.append(11)
+                    
+                else:
+                    
+                    det_list_final.append(int(det[1]))
+
+            elif 'b0' in det:
+                
+                det_list_final.append(12)
+                
+            elif 'b1' in det:
+                
+                det_list_final.append(13)
+
+            else:
+
+                raise Exception('Wrong format for detector selection')
+
+        self._use_dets = det_list_final
--- morgoth/bkg_fit_handler.py
+++ morgoth/bkg_fit_handler.py
@@ -1,10 +1,10 @@
 import luigi
 import os
 
-from morgoth.downloaders import DownloadTTEFile, DownloadTrigdat
+from morgoth.downloaders import DownloadTTEFile, DownloadTrigdat, DownloadCSPECFile
 from morgoth.time_selection_handler import TimeSelectionHandler
 
-from auto_loc.fit.prepare_fit import BkgFittingTrigdat
+from morgoth.auto_loc.bkg_fit import BkgFittingTrigdat, BkgFittingTTE
 
 base_dir = os.environ.get("GBM_TRIGGER_DATA_DIR")
 
@@ -29,27 +29,49 @@ _gbm_detectors = (
 class BackgroundFitTTE(luigi.ExternalTask):
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
-    detector = luigi.Parameter()
+    #detector = luigi.Parameter()
 
     def requires(self):
 
         return {'Time_Selection': TimeSelectionHandler(grb_name=self.grb_name),
-                'Data_File:': DownloadTTEFile(grb_name=self.grb_name,
-                                              version=self.version,
-                                              detector=self.detector)}
+                'Trigdat_Bkg':BackgroundFitTrigdat(grb_name=self.grb_name, version="v01"),#CHANGE THIS TO v00
+                'TTE_File': [DownloadTTEFile(grb_name=self.grb_name,
+                                            version=self.version,
+                                             detector=d) for d in _gbm_detectors],
+                'CSPEC_File':[DownloadCSPECFile(grb_name=self.grb_name,
+                                               version=self.version,
+                                               detector=d) for d in _gbm_detectors]}
     
     def output(self):
-
-        filename = f"tte_{self.version}_bkg_fit.txt"
-        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
+        
+        yml_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_tte_{self.version}.yml")
+        
+        return luigi.LocalTarget(yml_path)
 
     def run(self):
 
-        filename = f"tte_{self.version}_bkg_fit.txt"
-        tmp = os.path.join(base_dir, self.grb_name, filename)
 
-        os.system(f"touch {tmp}")
+        time_selection_filename = "time_selection.yml"
+        
+        time_selection_path = os.path.join(base_dir, self.grb_name, time_selection_filename)
 
+        bkg_trigdat_filename = "bkg_fit_trigdat_v01.yml" #TODO change this to v00
+
+        bkg_trigdat_path = os.path.join(base_dir, self.grb_name, bkg_trigdat_filename) 
+        
+        bkg_fit = BkgFittingTTE(self.grb_name, self.version, time_selection_path, bkg_trigdat_path)
+
+        bkg_files_dir = os.path.join(base_dir, self.grb_name, f"bkg_files_tte_{self.version}")
+
+        lightcurves_dir = os.path.join(base_dir, self.grb_name, f"lightcurves_tte_{self.version}")
+        
+        bkg_fit.save_bkg_file(bkg_files_dir)
+
+        bkg_fit.save_lightcurves(lightcurves_dir)
+
+        yml_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_tte_{self.version}.yml")
+
+        bkg_fit.save_yaml(yml_path)
     
 class BackgroundFitTrigdat(luigi.ExternalTask):
     grb_name = luigi.Parameter()
@@ -62,28 +84,28 @@ class BackgroundFitTrigdat(luigi.ExternalTask):
                                               version=self.version)}
 
     def output(self):
-        bkg_files_dir = os.path.join(base_dir, self.grb_name, "bkg_files")
 
-        lightcurves_dir = os.path.join(base_dir, self.grb_name, "lightcurves")
+        yml_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_trigdat_{self.version}.yml")
         
-        return {"Bkg_fits": [luigi.LocalTarget(os.path.join(bkg_files_dir, f"bkg_det{d}.h5")) for d in _gbm_detectors],
-                "Lightcurves": [luigi.LocalTarget(os.path.join(lightcurves_dir, f"{self.grb_name}_lightcurve_trigdat_detector_{d}_plot_{self.version}.png")) for d in _gbm_detectors]}
+        return luigi.LocalTarget(yml_path)
 
     def run(self):
 
-        time_selection_filename = "time_selection.json"
+        time_selection_filename = "time_selection.yml"
 
         time_selection_path = os.path.join(base_dir, self.grb_name, time_selection_filename)
         
         bkg_fit = BkgFittingTrigdat(self.grb_name, self.version, time_selection_path)
 
-        bkg_files_dir = os.path.join(base_dir, self.grb_name, "bkg_files")
+        bkg_files_dir = os.path.join(base_dir, self.grb_name, f"bkg_files_trigdat_{self.version}")
 
-        lightcurves_dir = os.path.join(base_dir, self.grb_name, "lightcurves")
+        lightcurves_dir = os.path.join(base_dir, self.grb_name, f"lightcurves_trigdat_{self.version}")
         
         bkg_fit.save_bkg_file(bkg_files_dir)
 
         bkg_fit.save_lightcurves(lightcurves_dir)
 
+        yml_path = os.path.join(base_dir, self.grb_name, f"bkg_fit_trigdat_{self.version}.yml")
 
+        bkg_fit.save_yaml(yml_path)
         
