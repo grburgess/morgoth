b7f03e9417fbf4e9ce7f67c336aa673be10736d8
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-03-26 18:05:28 +0100

    Add CreateAllLightcurves

--- morgoth/plots.py
+++ morgoth/plots.py
@@ -28,3 +28,38 @@ class CreateLightcurve(luigi.Task):
         tmp = os.path.join(base_dir, self.grb_name, self.report_type, self.version, 'plots', filename)
 
         os.system(f"touch {tmp}")
+
+
+class CreateAllLightcurves(luigi.Task):
+    grb_name = luigi.Parameter()
+    report_type = luigi.Parameter()
+    version = luigi.Parameter(default="v00")
+
+    def requires(self):
+        return {
+            "n0": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n0", version=self.version),
+            "n1": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n1", version=self.version),
+            "n2": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n2", version=self.version),
+            "n3": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n3", version=self.version),
+            "n4": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n4", version=self.version),
+            "n5": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n5", version=self.version),
+            "n6": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n6", version=self.version),
+            "n7": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n7", version=self.version),
+            "n8": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n8", version=self.version),
+            "n9": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="n9", version=self.version),
+            "na": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="na", version=self.version),
+            "nb": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="nb", version=self.version),
+            "b0": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="b0", version=self.version),
+            "b1": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="b1", version=self.version),
+            "b2": CreateLightcurve(grb_name=self.grb_name, report_type=self.report_type, detector="b2", version=self.version)
+        }
+
+    def output(self):
+        filename = f"{self.report_type}_{self.version}_plot_all_lightcurves.txt"
+        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, self.report_type, self.version, filename))
+
+    def run(self):
+        filename = f"{self.report_type}_{self.version}_plot_all_lightcurves.txt"
+        tmp = os.path.join(base_dir, self.grb_name, self.report_type, self.version, filename)
+
+        os.system(f"touch {tmp}")
