a6e9ed7f26ec108cffc8d0895ea284080373ba22
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-03-26 18:44:42 +0100

    Add result reader to ProcessFitResults

--- morgoth/balrog_handlers.py
+++ morgoth/balrog_handlers.py
@@ -1,12 +1,19 @@
 import luigi
 import os
+import yaml
+from datetime import datetime
+import shutil
+
+from morgoth.utils.file_utils import if_directory_not_existing_then_make
+from morgoth.utils.package_data import get_path_of_data_file
 from morgoth.utils.download_file import BackgroundDownload
 from morgoth.trigger import OpenGBMFile, GBMTriggerFile
 from morgoth.configuration import morgoth_config
+from morgoth.utils.env import get_env_value
 from morgoth.downloaders import DownloadTTEFile, DownloadTrigdat
+from morgoth.utils.result_reader import ResultReader
 
-base_dir = os.environ.get("GBM_TRIGGER_DATA_DIR")
-
+base_dir = get_env_value("GBM_TRIGGER_DATA_DIR")
 
 _gbm_detectors = (
     "n0",
@@ -27,7 +34,6 @@ _gbm_detectors = (
 
 
 class ProcessFitResults(luigi.Task):
-
     grb_name = luigi.Parameter()
     report_type = luigi.Parameter()
     version = luigi.Parameter(default="v00")
@@ -47,16 +53,34 @@ class ProcessFitResults(luigi.Task):
             return None
 
     def output(self):
+        base_job = os.path.join(base_dir, self.grb_name, self.report_type, self.version)
+        result_name = f"{self.report_type}_{self.version}_fit_result.yml"
 
-        filename = f"{self.report_type}_{self.version}_report.txt"
-        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
+        return {
+            'result': luigi.LocalTarget(os.path.join(base_job, result_name)),
+            'post_equal_weights': luigi.LocalTarget(os.path.join(base_job, 'chains', 'post_equal_weights.dat'))
+        }
 
     def run(self):
+        out_dir = os.path.join(base_dir, self.grb_name, self.report_type, self.version, 'chains')
+        if_directory_not_existing_then_make(out_dir)
+        shutil.copyfile(get_path_of_data_file('post_equal_weights.dat'), os.path.join(out_dir, 'post_equal_weights.dat'))
 
-        filename = f"{self.report_type}_{self.version}_report.txt"
-        tmp = os.path.join(base_dir, self.grb_name, filename)
+        result_path = f"{base_dir}/{self.grb_name}/{self.report_type}/{self.version}/fit_result/" \
+            f"{self.grb_name}_{self.report_type}_{self.version}_loc_results.fits"
 
-        os.system(f"touch {tmp}")
+        result_reader = ResultReader(
+            grb_name=self.grb_name,
+            report_type=self.report_type,
+            version=self.version,
+            result_file=result_path,
+            trigger_file='dummy'
+        )
+
+        filename = f"{self.report_type}_{self.version}_fit_result.yml"
+        file_path = os.path.join(base_dir, self.grb_name, self.report_type, self.version, filename)
+
+        result_reader.save_result_yml(file_path)
 
 
 class RunBalrogTTE(luigi.ExternalTask):
@@ -64,19 +88,16 @@ class RunBalrogTTE(luigi.ExternalTask):
     version = luigi.Parameter(default="v00")
 
     def requires(self):
-
         return [
             DownloadTTEFile(grb_name=self.grb_name, version=self.version, detector=d)
             for d in _gbm_detectors
         ]
 
     def output(self):
-
         filename = f"tte_{self.version}_fit.txt"
         return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
 
     def run(self):
-
         filename = f"tte_{self.version}_fit.txt"
         tmp = os.path.join(base_dir, self.grb_name, filename)
 
@@ -88,16 +109,13 @@ class RunBalrogTrigdat(luigi.ExternalTask):
     version = luigi.Parameter(default="v00")
 
     def requires(self):
-
         return DownloadTrigdat(grb_name=self.grb_name, version=self.version)
 
     def output(self):
-
         filename = f"trigdat_{self.version}_fit.txt"
         return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
 
     def run(self):
-
         filename = f"trigdat_{self.version}_fit.txt"
         tmp = os.path.join(base_dir, self.grb_name, filename)
 
