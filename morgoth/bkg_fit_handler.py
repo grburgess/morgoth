af15fb20ad264dd48bc5d6f759fc4e0f8e51453c
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-03-28 21:28:17 +0100

    Gather trigdat versions and store first found for tte fit

--- morgoth/bkg_fit_handler.py
+++ morgoth/bkg_fit_handler.py
@@ -1,6 +1,8 @@
 import os
+import time
 
 import luigi
+import yaml
 
 from morgoth.auto_loc.bkg_fit import BkgFittingTTE, BkgFittingTrigdat
 from morgoth.downloaders import DownloadCSPECFile, DownloadTTEFile, DownloadTrigdat
@@ -26,24 +28,24 @@ _gbm_detectors = (
 )
 
 
-class BackgroundFitTTE(luigi.ExternalTask):
+class BackgroundFitTTE(luigi.Task):
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
 
     def requires(self):
         return {
             'time_selection': TimeSelectionHandler(grb_name=self.grb_name),
-            'trigdat_bkg': BackgroundFitTrigdat(grb_name=self.grb_name, version="v01"),  # CHANGE THIS TO v00
-            'tte_file': [DownloadTTEFile(grb_name=self.grb_name,
-                                         version=self.version,
-                                         detector=d) for d in _gbm_detectors],
-            'cspec_file': [DownloadCSPECFile(grb_name=self.grb_name,
-                                             version=self.version,
-                                             detector=d) for d in _gbm_detectors]
+            'trigdat_version': GatherTrigdatBackgroundFit(grb_name=self.grb_name),
+            'tte_files': [DownloadTTEFile(grb_name=self.grb_name,
+                                          version=self.version,
+                                          detector=det) for det in _gbm_detectors],
+            'cspec_files': [DownloadCSPECFile(grb_name=self.grb_name,
+                                              version=self.version,
+                                              detector=det) for det in _gbm_detectors]
         }
 
     def output(self):
-        base_job = os.path.join(base_dir, self.grb_name, 'trigdat', self.version)
+        base_job = os.path.join(base_dir, self.grb_name, 'tte', self.version)
         return {
             'bkg_fit_yml': luigi.LocalTarget(os.path.join(base_job, f"bkg_fit_tte_{self.version}.yml")),
             'bkg_fit_files': [luigi.LocalTarget(os.path.join(base_job, 'bkg_files', f'bkg_det_{d}.h5'))
@@ -51,14 +53,24 @@ class BackgroundFitTTE(luigi.ExternalTask):
         }
 
     def run(self):
-        base_job = os.path.join(base_dir, self.grb_name, 'trigdat', self.version)
+        base_job = os.path.join(base_dir, self.grb_name, 'tte', self.version)
 
-        # Fit Background
+        # Get the first trigdat version and gather the result of the background
+        with self.input()['trigdat_version'].open() as f:
+            trigdat_version = yaml.safe_load(f)['trigdat_version']
+
+        trigdat_file = DownloadTrigdat(grb_name=self.grb_name, version=trigdat_version).output()
+        trigdat_bkg = BackgroundFitTrigdat(grb_name=self.grb_name, version=trigdat_version).output()
+
+        # Fit TTE background
         bkg_fit = BkgFittingTTE(
             self.grb_name,
             self.version,
+            trigdat_file=trigdat_file.path,
+            tte_files=self.input()['tte_files'],
+            cspec_files=self.input()['cspec_files'],
             time_selection_file_path=self.input()['time_selection'].path,
-            bkg_fitting_file_path=self.input()['trigdat_bkg'].path
+            bkg_fitting_file_path=trigdat_bkg['bkg_fit_yml'].path
         )
 
         # Save background fit
@@ -77,7 +89,56 @@ class BackgroundFitTTE(luigi.ExternalTask):
         )
 
 
-class BackgroundFitTrigdat(luigi.ExternalTask):
+class GatherTrigdatBackgroundFit(luigi.Task):
+    grb_name = luigi.Parameter()
+
+    def requires(self):
+        return {
+            'time_selection': TimeSelectionHandler(grb_name=self.grb_name),
+        }
+
+    def output(self):
+        return  luigi.LocalTarget(os.path.join(base_dir, self.grb_name, f"gather_trigdat_complete.yml"))
+
+    def run(self):
+        # the time spent waiting so far
+        time_spent = 0  # seconds
+
+        while True:
+            if BackgroundFitTrigdat(grb_name=self.grb_name, version="v00").complete():
+                version = 'v00'
+                break
+            else:
+                print('version 0 not complete')
+
+            if BackgroundFitTrigdat(grb_name=self.grb_name, version="v01").complete():
+                version = 'v01'
+                break
+            else:
+                print('version 1 not complete')
+
+            if BackgroundFitTrigdat(grb_name=self.grb_name, version="v02").complete():
+                version = 'v02'
+                break
+            else:
+                print('version 2 not complete')
+
+            time.sleep(2)
+
+            # up date the time we have left
+            time_spent += 2
+
+        print(f'The total waiting time was: {time_spent}')
+
+        version_dict = {
+            'trigdat_version': version
+        }
+
+        with self.output().open('w') as f:
+            yaml.dump(version_dict, f, default_flow_style=False)
+
+
+class BackgroundFitTrigdat(luigi.Task):
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
 
@@ -98,7 +159,7 @@ class BackgroundFitTrigdat(luigi.ExternalTask):
     def run(self):
         base_job = os.path.join(base_dir, self.grb_name, 'trigdat', self.version)
 
-        # Fit Background
+        # Fit Trigdat background
         bkg_fit = BkgFittingTrigdat(
             self.grb_name,
             self.version,
