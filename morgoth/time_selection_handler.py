82d357e5e8db0d68d0af3be69ffc74f828408b8f
Tobias Preis <tobias.preis@tum.de>
2023-07-03 14:00:28 +0200

    correct req for tte timeselection

--- morgoth/time_selection_handler.py
+++ morgoth/time_selection_handler.py
@@ -17,38 +17,67 @@ class TimeSelectionHandler(luigi.Task):
     def requires(self):
         # return GatherTrigdatDownload(grb_name=self.grb_name)
         if self.report_type == "trigdat":
-            return DownloadTrigdat(grb_name =self.grb_name, version = self.version)
-
+            return DownloadTrigdat(grb_name=self.grb_name, version=self.version)
+        elif self.report_type == "tte":
+            with open(GatherTrigdatDownload.output(), "r") as f:
+                trigdat_complete = yaml.safe_load(f)
+                trigdat_version = trigdat_complete[trigdat_version]
+            return TimeSelectionHandler(
+                grb_name=self.grb_name, version=trigdat_version, report_type="trigdat"
+            )
 
     def output(self):
         filename = f"time_selection_{self.version}.yml"
 
-        return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, self.report_type ,filename))
+        return luigi.LocalTarget(
+            os.path.join(base_dir, self.grb_name, self.report_type, filename)
+        )
 
     def run(self):
         """
         with self.input().open() as f:
             trigdat_version = yaml.safe_load(f)["trigdat_version"]
         """
-        trigdat_versions = ["v02","v01","v00"]
+        trigdat_versions = ["v02", "v01", "v00"]
         if self.report_type == "trigdat":
             tf_name = f"glg_trigdat_all_bn{self.grb_name[3:]}_{self.version}.fit"
 
-            trigdat_file = os.path.join(base_dir,self.grb_name,"trigdat",tf_name)
+            trigdat_file = os.path.join(base_dir, self.grb_name, "trigdat", tf_name)
 
             time_selection = TimeSelectionBB(
                 grb_name=self.grb_name, trigdat_file=trigdat_file, fine=True
             )
 
             time_selection.save_yaml(
-                os.path.join(base_dir, self.grb_name,self.report_type,f"time_selection_{self.version}.yml")
+                os.path.join(
+                    base_dir,
+                    self.grb_name,
+                    self.report_type,
+                    f"time_selection_{self.version}.yml",
+                )
             )
         elif self.report_type == "tte":
             for tv in trigdat_versions:
                 try:
-                    with open(os.path.join(base_dir, self.grb_name,"trigdat",f"time_selection_{tv}.yml"),"r") as f:
+                    with open(
+                        os.path.join(
+                            base_dir,
+                            self.grb_name,
+                            "trigdat",
+                            f"time_selection_{tv}.yml",
+                        ),
+                        "r",
+                    ) as f:
                         con = yaml.safe_load(f)
-                    with open(os.path.join(base_dir,self.grb_name,"tte",f"time_selection_{self.version}.yml"),"w+") as f:
-                        yaml.dump(con,f)
+                    with open(
+                        os.path.join(
+                            base_dir,
+                            self.grb_name,
+                            "tte",
+                            f"time_selection_{self.version}.yml",
+                        ),
+                        "w+",
+                    ) as f:
+                        yaml.dump(con, f)
                 except FileNotFoundError:
                     pass
